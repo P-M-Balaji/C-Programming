c5813d1aca35cf55796654db4e6d1859f0ff4180
Balaji PM <45323249+P-M-Balaji@users.noreply.github.com>
2021-03-25 18:35:25 +0530

    Pointers2 - Initial commit

---
+++ pointers2.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    int *p = a, i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d\t", &a[i]);
+        printf("%d\t", &p[i]);
+        printf("%d\t", p + i);
+        printf("%d\t", &i[a]);
+        printf("%d\t", &p[i]);
+        printf("%d\n", &i[p]);
+    }
+    return 0;
+}
